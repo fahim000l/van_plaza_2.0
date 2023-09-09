8b0ddca2e68a951afda68f544c30a0d4b1f4dcc1
Fahim Faisal <mdfahimfaisal000@gmail.com>
2023-09-10 02:15:15 +0600

    style engine

--- pages/_app.js
+++ pages/_app.js
@@ -5,22 +5,25 @@ import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
+import { StyledEngineProvider } from "@mui/material";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <StockInProvider>
-            <AddProductProvider>
-              <Component {...pageProps} />
-              <Toaster position="top-center" />
-            </AddProductProvider>
-          </StockInProvider>
-        </AuthProvider>
-      </QueryClientProvider>
-    </SessionProvider>
+    <StyledEngineProvider injectFirst>
+      <SessionProvider session={pageProps.session}>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <StockInProvider>
+              <AddProductProvider>
+                <Component {...pageProps} />
+                <Toaster position="top-center" />
+              </AddProductProvider>
+            </StockInProvider>
+          </AuthProvider>
+        </QueryClientProvider>
+      </SessionProvider>
+    </StyledEngineProvider>
   );
 }
