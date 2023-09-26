c19475be27f9cf7f328610dce8140deaff49176b
Fahim Faisal <mdfahimfaisal000@gmail.com>
2023-09-26 15:51:52 +0600

    location

--- components/LocationSelectModal.jsx
+++ components/LocationSelectModal.jsx
@@ -1,3 +1,68 @@
+// import React, { useEffect, useRef, useState } from "react";
+// import * as maptilersdk from "@maptiler/sdk";
+// import { Country, State, City, ICity } from "country-state-city";
+// import { divisions } from "@/bangladesh-geojson/bd-divisions";
+// import { districts } from "@/bangladesh-geojson/bd-districts";
+// import { upoZilas } from "@/bangladesh-geojson/bd-upazilas";
+// import { postCodes } from "@/bangladesh-geojson/bd-postcodes";
+// import AutoSelect from "./common_auto-complete";
+
+// const LocationSelectModal = () => {
+//   console.log(divisions);
+//   console.log(districts?.districts?.filter((dis) => dis?.division_id === "2"));
+//   console.log(upoZilas?.upazilas?.filter((upo) => upo?.district_id === "43"));
+//   console.log(
+//     postCodes?.postcodes?.filter((post) => post?.upazila === "Chittagong Sadar")
+//   );
+
+//   const [places, setPlaces] = useState(null);
+//   const [query, setQuery] = useState("");
+
+//   useEffect(() => {
+//     if (query) {
+//       fetch(
+//         `https://barikoi.xyz/v1/api/search/verify/autocomplete/bkoi_b98a765d5bf3797e9d766f55da7ff932774f2886d7059c6b889be5aa2e547b12/place?q=House%${query}`
+//       )
+//         .then((res) => res.json())
+//         .then((data) => {
+//           console.log(data);
+//           setPlaces(data?.places);
+//         });
+//     } else {
+//       setPlaces(null);
+//     }
+//   }, [query]);
+
+//   return (
+//     <div>
+//       <input
+//         type="checkbox"
+//         id="locationSelectModal"
+//         className="modal-toggle"
+//       />
+//       <div className="modal modal-bottom">
+//         <div className="modal-box p-0">
+//           <input
+//             onChange={(e) => setQuery(e.target.value)}
+//             type="text"
+//             className="input input-border"
+//           />
+//           <div>
+//             {places?.map((place) => (
+//               <p>{place?.address}</p>
+//             ))}
+//           </div>
+//         </div>
+//         <label className="modal-backdrop" htmlFor="locationSelectModal">
+//           Close
+//         </label>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default LocationSelectModal;
+
 import {
   Combobox,
   ComboboxInput,
@@ -6,9 +71,11 @@ import {
   ComboboxPopover,
 } from "@reach/combobox";
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import usePlacesAutocomplete from "use-places-autocomplete";
 import { LocationOn } from "@mui/icons-material";
+import AutoSelect from "./common_auto-complete";
+import { Button, IconButton, ListItemButton } from "@mui/joy";
 
 const LocationSelectModal = () => {
   const { isLoaded } = useLoadScript({
@@ -39,48 +106,81 @@ export default LocationSelectModal;
 
 function Map() {
   const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
-  const [selectedPlace, setSelectedPlace] = useState();
+  const [selectedPlace, setSelectedPlace] = useState(null);
 
   return (
     <>
       <div className="w-full">
-        <PlacesAutoComplete selectedPlace={selectedPlace} />
+        <PlacesAutoComplete setSelectedPlace={setSelectedPlace} />
       </div>
       <GoogleMap
         zoom={10}
         center={center}
         mapContainerClassName="w-full h-[50vh]"
       >
-        <Marker icon={<LocationOn />} position={center} />
+        <Marker icon={<LocationOn />} position={selectedPlace || center} />
       </GoogleMap>
     </>
   );
 }
 
-function PlacesAutoComplete({ selectedPlace }) {
-  const {
-    ready,
-    value,
-    setValue,
-    clearSuggestions,
-    suggestions: { status, data },
-  } = usePlacesAutocomplete();
+function PlacesAutoComplete({ setSelectedPlace }) {
+  const [places, setPlaces] = useState(null);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://barikoi.xyz/v1/api/search/verify/autocomplete/bkoi_b98a765d5bf3797e9d766f55da7ff932774f2886d7059c6b889be5aa2e547b12/place?q=House%${query}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPlaces(data?.places);
+      });
+  }, [query]);
+
+  return (
+    <div>
+      <input
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+        className="input input-border w-full"
+        placeholder="Search your location"
+      />
+      <div className="grid grid-cols-1 text-start">
+        {places?.map(({ id, address, latitude, longitude }) => (
+          <ListItemButton
+            onClick={() =>
+              setSelectedPlace({
+                lat: parseFloat(latitude),
+                lng: parseFloat(longitude),
+              })
+            }
+            key={id}
+            className="text-black font-bold text-start p-2"
+          >
+            <IconButton>
+              <LocationOn />
+            </IconButton>
+            {address}
+          </ListItemButton>
+        ))}
+      </div>
+    </div>
+  );
 
   return (
     <Combobox>
       <ComboboxInput
-        value={value}
-        disabled={!ready}
         className="input input-border w-full"
         placeholder="Search Your Location"
-        onChange={(event) => setValue(event.target.value)}
+        onChange={(event) => setQuery(event.target.value)}
       />
-      <ComboboxPopover>
+      <ComboboxPopover className="bg-white text-black relative z-[200]">
         <ComboboxList>
-          {status === "OK" &&
-            data?.map(({ place_id, description }) => (
-              <ComboboxOption key={place_id} value={description} />
-            ))}
+          {places?.map(({ address }, i) => (
+            <ComboboxOption key={i} value={address} />
+          ))}
         </ComboboxList>
       </ComboboxPopover>
     </Combobox>
